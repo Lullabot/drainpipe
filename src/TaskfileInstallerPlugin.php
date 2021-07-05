b7ca5d6d92f30a4c5806ee4984dfce97fe271242
Sally Young <git@justa.fish>
2021-07-05 19:55:24 +0100

    Integrate lullabot/drainpipe-installer into this repository

---
+++ src/TaskfileInstallerPlugin.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lullabot\Drainpipe;
+
+use Composer\Composer;
+use Composer\Config;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use Composer\Util\Filesystem;
+
+class TaskfileInstallerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    /**
+     * @var IOInterface
+     */
+    protected $io;
+
+    /**
+     * Composer instance configuration.
+     *
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->io = $io;
+        $this->config = $composer->getConfig();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::POST_INSTALL_CMD => 'onPostInstallCmd',
+            ScriptEvents::POST_UPDATE_CMD => 'onPostUpdateCmd',
+        ];
+    }
+
+    /**
+     * Handle post install command events.
+     *
+     * @param Event $event the event to handle
+     */
+    public function onPostInstallCmd(Event $event)
+    {
+        $this->installTaskfile();
+        $this->installGitignore();
+    }
+
+    /**
+     * Handle post update command events.
+     *
+     * @param event $event The event to handle
+     */
+    public function onPostUpdateCmd(Event $event)
+    {
+        $this->installTaskfile();
+        $this->installGitignore();
+    }
+
+    /**
+     * Copies Taskfile.yml from the scaffold directory if it doesn't yet exist.
+     */
+    private function installTaskfile(): void
+    {
+        $vendor = $this->config->get('vendor-dir');
+        $taskfilePath = $vendor.'/lullabot/drainpipe/scaffold/Taskfile.yml';
+
+        if (!file_exists('./Taskfile.yml')) {
+            $this->io->write('<info>Creating initial Taskfile.yml...</info>');
+            $fs = new Filesystem();
+            $fs->copy(
+                $taskfilePath,
+                './Taskfile.yml'
+            );
+        } else {
+            $distHash = sha1_file($taskfilePath);
+            $repoHash = sha1_file('./Taskfile.yml');
+            if ($distHash !== $repoHash) {
+                $this->io->warning(
+                    'Taskfile.yml has either been customized or requires review.'
+                );
+                $this->io->warning(
+                    sprintf(
+                        'Compare Taskfile.yml in the root of your repository with %s and update as needed.',
+                        $taskfilePath
+                    )
+                );
+            }
+        }
+    }
+
+    /**
+     * Copies gitignore from the scaffold directory if it doesn't yet exist.
+     */
+    private function installGitignore(): void
+    {
+        $vendor = $this->config->get('vendor-dir');
+        $gitignorePath = $vendor.'/lullabot/drainpipe/scaffold/gitignore';
+        if (!file_exists('./.gitignore')) {
+            $this->io->write('<info>Creating initial .gitignore...</info>');
+            $fs = new Filesystem();
+            $fs->copy($gitignorePath, './.gitignore');
+        } else {
+            $contents = file_get_contents('./.gitignore');
+            if (!strpos('.task', $contents)) {
+                $this->io->warning(
+                    sprintf(
+                    '.gitignore does not contain drainpipe ignores. Compare .gitignpre in the root of your repository with %s and update as needed.',
+                        $gitignorePath
+                    )
+                );
+            }
+        }
+    }
+}
