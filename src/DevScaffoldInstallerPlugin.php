441be069b550cc0039fdca3bf9ed41fa2edf4473
Sally Young <git@justa.fish>
2021-07-16 22:08:21 +0100

    setTimeout(0) on yarn install

--- src/DevScaffoldInstallerPlugin.php
+++ src/DevScaffoldInstallerPlugin.php
@@ -208,6 +208,7 @@ class DevScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInte
         // Add dependencies.
         if (file_exists('yarn.lock')) {
             $yarn = new Process(array_merge(['yarn', 'add'], $dependencies, ['--dev']));
+            $yarn->setTimeout(0);
             $yarn->run(function($type, $buffer) {
                 if (Process::ERR === $type) {
                     $this->io->write('ERR > '.$buffer);
@@ -217,6 +218,7 @@ class DevScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInte
             });
         } else if (file_exists('package-lock.json')) {
             $npm = new Process(array_merge(['npm', 'install'], $dependencies, ['--save-dev']));
+>           $npm->setTimeout(0);
             $npm->run(function($type, $buffer) {
                 if (Process::ERR === $type) {
                     $this->io->write('ERR > '.$buffer);
