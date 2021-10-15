2033e47d901046195a0da987bb955e350a69a795
Sally Young <git@justa.fish>
2021-10-15 17:41:03 +0100

    Use gulp for Sass builds so autoprefixer and cssnano can be used (#41)

--- config/esbuild.js
+++ config/esbuild.js
@@ -1,92 +1,132 @@
-const yargs = require('yargs');
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
-const { build } = require('esbuild');
-const chokidar = require('chokidar');
-const { pnpPlugin } = require('@yarnpkg/esbuild-plugin-pnp');
-const scripts = argv.files.split(' ');
-
-
-if (!scripts.length) {
-  console.log('No files to compile');
-  process.exit(0);
+const fs = require('fs');
+const path = require('path');
+let yarn2 = false;
+try {
+  const { resolveToUnqualified } = require('pnpapi');
+  yarn2 = true;
+}
+catch (error) {
+  // Nothing
 }
 
-const targets = [];
-const sources = [];
+// So they can be easily printed to the user.
+const deps = [
+  'chokidar',
+  'esbuild',
+  'yargs',
+].join(' ');
+
+try {
+  const yargs = require('yargs');
+  const { hideBin } = require('yargs/helpers')
+  const argv = yargs(hideBin(process.argv)).argv
+  const { build } = require('esbuild');
+  const chokidar = require('chokidar');
+  const scripts = argv.files.split(' ');
+
+
+  if (!scripts.length) {
+    console.log('No files to compile');
+    process.exit(0);
+  }
+
+  const targets = [];
+  const sources = [];
 
 // Check that every source/target has the same basedir (probably "web") due to
 // being unable to provide separate entryNames.
 // See https://github.com/evanw/esbuild/issues/224
-const baseDirs = [];
+  const baseDirs = [];
 // Check that the output script name matches the same as the input script name,
 // due to the same limitation above. It can have a different file extension,
 // e.g. web/modules/custom/mymodule/script.js:web/modules/custom/mymodule/script.min.js
-const fileExtensions = [];
-scripts.forEach(script => {
-  const [source, target] = script.split(':');
-  sources.push(source);
-  targets.push(target);
-  baseDirs.push(source.split('/')[0]);
-  baseDirs.push(target.split('/')[0]);
-  const sourceFile = source.split('/').pop();
-  const targetFile = target.split('/').pop();
-  fileExtensions.push(targetFile.split('.', 2)[1]);
-});
-const uniqueBaseDir = [...new Set(baseDirs)];
-if (uniqueBaseDir.length !== 1) {
-  console.error('All source and target files must have a root directory in common');
-  process.exit(1);
-}
-const uniqueFileExtension = [ ...new Set(fileExtensions)];
-if (uniqueFileExtension.length !== 1) {
-  console.error('All target files must have the same file extension e.g. "min.js"');
-  process.exit(1);
-}
-
-
-  (async() => {
-  try {
-    let builder = await build({
-      plugins: [pnpPlugin()],
-      entryPoints: scripts.map(script => script.split(':')[0]),
-      outdir: uniqueBaseDir[0],
-      entryNames: `[dir]/[name].${uniqueFileExtension[0]}`,
-      bundle: true,
-      sourcemap: true,
-      minify: !!argv.minify,
-      incremental: !!argv.watch,
-      logLevel: 'info',
-    });
-
-    if (!!argv.watch) {
-      let ready = false;
-      chokidar.watch(`${uniqueBaseDir[0]}/**/*.js`, {
-        ignored: targets,
-      })
-        .on('ready', () => {
-          ready = true;
-          console.log('[esbuild]', 'watching for changes');
-        })
-        .on('all', (event, path) => {
-          if (ready) {
-            console.log('[esbuild]', event, path);
-            builder.rebuild()
-              .then(result => {
-                console.log('[esbuild]', 'watch build succeeded')
-                if (result.warnings.length) {
-                  console.warn('[esbuild]', 'watch build has warnings', result.warnings);
-                }
-              })
-              .catch(err => {
-                console.error('[esbuild]', 'watch build failed', err);
-              });
-          }
-        });
-    }
-  }
-  catch(err) {
-    console.error(err);
+  const fileExtensions = [];
+  scripts.forEach(script => {
+    const [source, target] = script.split(':');
+    sources.push(source);
+    targets.push(target);
+    baseDirs.push(source.split('/')[0]);
+    baseDirs.push(target.split('/')[0]);
+    const sourceFile = source.split('/').pop();
+    const targetFile = target.split('/').pop();
+    fileExtensions.push(targetFile.split('.', 2)[1]);
+  });
+  const uniqueBaseDir = [...new Set(baseDirs)];
+  if (uniqueBaseDir.length !== 1) {
+    console.error('All source and target files must have a root directory in common');
     process.exit(1);
   }
-})();
+  const uniqueFileExtension = [...new Set(fileExtensions)];
+  if (uniqueFileExtension.length !== 1) {
+    console.error('All target files must have the same file extension e.g. "min.js"');
+    process.exit(1);
+  }
+
+
+  (async () => {
+    try {
+      const plugins = [];
+      if (yarn2) {
+        const { pnpPlugin } = require('@yarnpkg/esbuild-plugin-pnp');
+        plugins.push(pnpPlugin);
+      }
+      let builder = await build({
+        plugins: [plugins],
+        entryPoints: scripts.map(script => script.split(':')[0]),
+        outdir: uniqueBaseDir[0],
+        entryNames: `[dir]/[name].${uniqueFileExtension[0]}`,
+        bundle: true,
+        sourcemap: true,
+        minify: !!argv.minify,
+        incremental: !!argv.watch,
+        logLevel: 'info',
+      });
+
+      if (!!argv.watch) {
+        let ready = false;
+        chokidar.watch(`${uniqueBaseDir[0]}/**/*.js`, {
+          ignored: targets,
+        })
+          .on('ready', () => {
+            ready = true;
+            console.log('[esbuild]', 'watching for changes');
+          })
+          .on('all', (event, path) => {
+            if (ready) {
+              console.log('[esbuild]', event, path);
+              builder.rebuild()
+                .then(result => {
+                  console.log('[esbuild]', 'watch build succeeded')
+                  if (result.warnings.length) {
+                    console.warn('[esbuild]', 'watch build has warnings', result.warnings);
+                  }
+                })
+                .catch(err => {
+                  console.error('[esbuild]', 'watch build failed', err);
+                });
+            }
+          });
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  })();
+}
+catch (error) {
+  if (error.code !== 'MODULE_NOT_FOUND') {
+    throw error;
+  }
+  console.error('🪠 Missing node dependency! Please run:');
+  if (fs.existsSync(path.join(process.cwd(), `package-lock.json`))) {
+    console.error(`npm install ${deps} --save-dev`);
+  }
+  else if (fs.existsSync(path.join(process.cwd(), 'yarn.lock'))) {
+    console.error(`yarn add ${deps} @yarnpkg/esbuild-plugin-pnp --dev`);
+  }
+  else {
+    console.error('yarn init');
+    console.error(`yarn add ${deps} @yarnpkg/esbuild-plugin-pnp --dev`);
+  }
+  process.exit(1);
+}
