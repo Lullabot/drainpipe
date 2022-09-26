1556de568fe721448297baf9707bf276b554e4d3
beto-aveiga <luis.aveiga@gmail.com>
2022-09-26 13:43:52 -0500

    Remove debugging

--- scaffold/github/actions/common/composer-lock-diff-comment/script.js
+++ scaffold/github/actions/common/composer-lock-diff-comment/script.js
@@ -1,29 +1,5 @@
 module.exports = async ({github, context}) => {
 
-  console.log('================================');
-  console.log('============== github ===============');
-  console.log('================================');
-  console.log(github);
-  console.log('================================');
-  console.log('============= context =============');
-  console.log('================================');
-  console.log(context);
-  console.log('================================');
-  console.log('============= payload ================');
-  console.log('================================');
-  console.log(context.payload);
-  console.log('================================');
-  console.log('================================');
-  console.log('================================');
-
-  console.log('============== owner =============');
-  console.log(context.repo.owner);
-  console.log('================================');
-  console.log('============== context.repo =============');
-  console.log(context.repo);
-  console.log('================================');
-  console.log('================================');
-
   const issues = await github.rest.pulls.list({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -51,13 +27,13 @@ module.exports = async ({github, context}) => {
     })
   })
 
+  const default_branch = context.payload.repository.default_branch
   // https://stackoverflow.com/a/52575123/1038565
   const execSync = require('child_process').execSync;
-
   execSync('composer global require davidrjonas/composer-lock-diff:^1.0');
-  execSync(`git fetch origin ${context.payload.repository.default_branch}`);
+  execSync(`git fetch origin ${default_branch}`);
 
-  const output = execSync('~/.composer/vendor/bin/composer-lock-diff --from=origin/main --md', { encoding: 'utf-8' });
+  const output = execSync(`~/.composer/vendor/bin/composer-lock-diff --from=origin/${default_branch}} --md`, { encoding: 'utf-8' });
 
   if (!output) {
     return
