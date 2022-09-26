9a3ac1af67ea64f69b76e55b874118bc07a4c5b4
beto-aveiga <luis.aveiga@gmail.com>
2022-09-26 10:58:56 -0500

    Composer lock diff comment fixes

--- src/ScaffoldInstallerPlugin.php
+++ src/ScaffoldInstallerPlugin.php
@@ -247,6 +247,16 @@ class ScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInterfa
                         }
                     }
                 }
+
+                if ($github === 'ComposerLockDiffComment') {
+                    $fs->ensureDirectoryExists('./.github/actions/drainpipe/composer-lock-diff-comment');
+                    $fs->copy("$scaffoldPath/github/actions/drainpipe/composer-lock-diff-comment", './.github/actions/drainpipe/drainpipe/post-composer-lock-diff-comment');
+
+                    if (!file_exists('./.github/workflows/ComposerLockDiffComment.yml')) {
+                        $fs->copy("$scaffoldPath/github/workflows/ComposerLockDiffComment.yml", './.github/workflows/ComposerLockDiffComment.yml');
+                    }
+                }
+
             }
         }
     }
