19710f5b9dfaac789f6b561c240e226eea136dd9
beto-aveiga <luis.aveiga@gmail.com>
2022-09-27 08:49:42 -0500

    Adding comments to explain the code

--- scaffold/github/actions/common/composer-lock-diff-comment/script.js
+++ scaffold/github/actions/common/composer-lock-diff-comment/script.js
@@ -1,5 +1,7 @@
 module.exports = async ({github, context}) => {
 
+  // Getting this PR's id.
+  // https://github.com/actions/github-script/issues/203#issuecomment-1027833024
   const issues = await github.rest.pulls.list({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -9,16 +11,19 @@ module.exports = async ({github, context}) => {
 
   const pr = context.issue.number || issues.data[0].number;
 
+  // Getting the comments on this PR.
   const comments = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: pr
   })
 
+  // Getting our previous comment.
   const composerLockDiffComments = comments.data.filter(comment => {
     return comment.body.indexOf("#composer-lock-diff-comment") != -1
   })
 
+  // Delete our previous comment, if any.
   composerLockDiffComments.map(function (comment) {
     github.rest.issues.deleteComment({
       owner: context.repo.owner,
@@ -28,6 +33,8 @@ module.exports = async ({github, context}) => {
   })
 
   const default_branch = context.payload.repository.default_branch;
+
+  // How to execute shell command in Javascript
   // https://stackoverflow.com/a/52575123/1038565
   const execSync = require('child_process').execSync;
   execSync('composer global require davidrjonas/composer-lock-diff:^1.0');
@@ -35,10 +42,12 @@ module.exports = async ({github, context}) => {
 
   const output = execSync(`~/.composer/vendor/bin/composer-lock-diff --from=origin/${default_branch} --md`);
 
+  // If there are no packages differences, do not post a new comment.
   if (!output) {
     return
   }
 
+  // Posting a comment with the composer-lock-diff output on this PR.
   github.rest.issues.createComment({
     issue_number: pr,
     owner: context.repo.owner,
