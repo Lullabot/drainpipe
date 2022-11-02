675d97c92d051b1b0e33f5738d3c1e390348ed20
Sally Young <git@justa.fish>
2022-11-02 15:58:11 +0000

    Fixes copying docker env file over (#145)

--- src/ScaffoldInstallerPlugin.php
+++ src/ScaffoldInstallerPlugin.php
@@ -173,7 +173,7 @@ class ScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInterfa
             $fs->copy($ddevCommandPath, './.ddev/commands/web/task');
 
             # Enable .env file support via docker-composer web environment.
-            $fs->copy($vendor.'/lullabot/drainpipe/scaffold/ddev/docker-compose-.env-file.yaml', './.ddev');
+            $fs->copy($vendor.'/lullabot/drainpipe/scaffold/ddev/docker-compose.env-file.yaml', './.ddev/docker-compose.env-file.yaml');
             if (!is_file('./.env')) {
                 $fs->copy($vendor.'/lullabot/drainpipe/scaffold/ddev/env', './.env');
             }
