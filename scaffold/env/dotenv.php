9d0a887ee16cea43f2f36c4753bf7ecddb2c5907
Sally Young <git@justa.fish>
2022-11-28 16:35:21 +0000

    Switch to phpdotenv >= 4

--- scaffold/env/dotenv.php
+++ scaffold/env/dotenv.php
@@ -11,12 +11,5 @@
 use Dotenv\Dotenv;
 
 $cwd = getcwd();
-
-// Load both .env.defaults and .env so we can use the immutable class.
-// Otherwise when we load .env after .env.defaults it's not able to overwrite
-// any variables, however loading with the mutable class means real environment
-// variables won't take precedence.
-$defaults = file_get_contents(join(DIRECTORY_SEPARATOR, [$cwd, '.env.defaults']));
-$overrides = file_get_contents(join(DIRECTORY_SEPARATOR, [$cwd, '.env']));
-
-Dotenv::parse($defaults . "\n" . "$overrides");
+$dotenv = Dotenv::createUnsafeImmutable($cwd, ['.env', '.env.defaults`'], false);
+$dotenv->load();
