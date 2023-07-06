06e9f82d01353c604f2bfd41d003f84ae736f2ec
Sally Young <git@justa.fish>
2023-07-06 13:59:02 +0100

    Fix file moves

--- .tugboat/steps/init.sh
+++ .tugboat/steps/init.sh
@@ -35,7 +35,8 @@ shopt -s dotglob
 mkdir ../drainpipe-tmp
 mv * ../drainpipe-tmp/
 composer create-project drupal/recommended-project .
-mv ../drainpipe-tmp/* .
+mkdir drainpipe
+mv ../drainpipe-tmp/* ./drainpipe/
 composer config extra.drupal-scaffold.gitignore true
 composer config --json extra.drupal-scaffold.allowed-packages \[\"lullabot/drainpipe\"]
 composer config --no-plugins allow-plugins.composer/installers true
