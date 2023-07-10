142b9bafa14addacb2e24dc3790bbcf0f4c86d93
Sally Young <git@justa.fish>
2023-07-10 13:02:52 +0100

    .

--- src/ScaffoldInstallerPlugin.php
+++ src/ScaffoldInstallerPlugin.php
@@ -324,8 +324,8 @@ class ScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInterfa
                 if (file_exists('./web/sites/default/settings.php')) {
                     $settings = file_get_contents('./web/sites/default/settings.php');
                     if (strpos($settings, 'settings.tugboat.php') === false) {
-                        //$include = 'include __DIR__ . "/settings.tugboat.php";';
-                        //file_put_contents('./web/sites/default/settings.php', $include . PHP_EOL, FILE_APPEND);
+                        $include = 'include __DIR__ . "/settings.tugboat.php";';
+                        file_put_contents('./web/sites/default/settings.php', $include . PHP_EOL, FILE_APPEND);
                     }
                 }
             }
