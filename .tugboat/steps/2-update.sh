ad5bb90897968e0d83cc9092db87e1dcbe34b909
Sally Young <git@justa.fish>
2023-07-27 11:08:12 +0100

    Fix Tugboat files directory permissions (#232)

--- .tugboat/steps/2-update.sh
+++ .tugboat/steps/2-update.sh
@@ -8,6 +8,11 @@ mv .tugboat .tugboat-tmp
 #drainpipe-end
 composer install
 ./vendor/bin/task sync
+
+# Set file permissions such that Drupal will not complain.
+chgrp -R www-data "${DOCROOT}/sites/default/files"
+find "${DOCROOT}/sites/default/files" -type d -exec chmod 2775 {} \;
+find "${DOCROOT}/sites/default/files" -type f -exec chmod 0664 {} \;
 #drainpipe-start
 ./vendor/bin/drush config:export --yes
 rm -rf .tugboat
