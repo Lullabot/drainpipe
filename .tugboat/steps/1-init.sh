61f3a592cc3b66be1847d2b002c1fcbf25b9bc9f
Andrew Berry <andrew.berry@lullabot.com>
2023-09-13 20:13:19 -0400

    Fix copied template

--- .tugboat/steps/1-init.sh
+++ .tugboat/steps/1-init.sh
@@ -46,7 +46,7 @@ apt-get install -y imagemagick
 apt-get install -y ca-certificates gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR={{ nodejs_version }}
+NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 apt-get update
 apt-get -qq install nodejs
