94ed49eca8b74521737ca19e4942cd95c704bc31
Andrew Berry <andrew.berry@lullabot.com>
2023-09-29 08:40:39 -0400

    Need more espresso apparently

--- src/ScaffoldInstallerPlugin.php
+++ src/ScaffoldInstallerPlugin.php
@@ -318,7 +318,7 @@ class ScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInterfa
                 'build_command' => 'build',
                 'update_command' => 'drupal:update',
                 'init' => [],
-                'task_path' => ($this->extra['drainpipe']['global-binaries']['task'] ?? 'task') ? : './vendor/bin/task',
+                'task_path' => isset($this->extra['drainpipe']['global-binaries']['task']) && $this->extra['drainpipe']['global-binaries']['task'] ? 'task' : './vendor/bin/task',
                 'task_version' => $binaryInstallerPlugin->getBinaryVersion('task'),
                 'pantheon' => isset($this->extra['drainpipe']['tugboat']['pantheon']),
             ];
