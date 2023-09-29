1fdc4916d8b5b7b0a146541d6e5d0bbe28ee5d9e
Andrew Berry <andrew.berry@lullabot.com>
2023-09-29 11:07:32 -0400

    Remove testing changes

--- .tugboat/steps/1-init.sh
+++ .tugboat/steps/1-init.sh
@@ -47,12 +47,11 @@ apt-get install -y ca-certificates gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=18
-NODE_MAJOR=16
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 apt-get update
 apt-get -qq install nodejs
 # This only works for node > 16, but that version is unsupported now anyway.
-#corepack enable
+corepack enable
 
 #drainpipe-start
 # This is necessary for testing as this repository doesn't hold a Drupal site.
