90d8872e47eaf2074e7ebd1ca76df2c1c055f56e
Sally Young <git@justa.fish>
2023-10-24 16:48:26 +0100

    switch to node 16

--- .tugboat/steps/1-init.sh
+++ .tugboat/steps/1-init.sh
@@ -46,7 +46,7 @@ apt-get install -y imagemagick
 apt-get install -y ca-certificates gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=18
+NODE_MAJOR=16
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # Ensure we use nodesource packages, even if Debian ships newer major versions.
