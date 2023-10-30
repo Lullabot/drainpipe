7f53fdfc87edf9389b4788163746c83e7095802f
Sally Young <git@justa.fish>
2023-10-30 13:40:59 +0000

    Fix tests?

--- tests/fixtures/metapackages/js/script.js
+++ tests/fixtures/metapackages/js/script.js
@@ -1,4 +1,4 @@
-import mobileNav from "./js/mobile-nav";
+import mobileNav from "./mobile-nav";
 // Ensures correct colors are shown for :active state of buttons on iOS.
 document.addEventListener("touchstart", function () {}, false);
 mobileNav(Drupal, once);
