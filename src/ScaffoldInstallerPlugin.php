24eba253f68ea44371d4273e86cfc43a3779b508
Sally Young <git@justa.fish>
2023-11-29 10:30:47 +0000

    Add GitLab CI / DDEV support (#339)

--- src/ScaffoldInstallerPlugin.php
+++ src/ScaffoldInstallerPlugin.php
@@ -224,12 +224,20 @@ class ScaffoldInstallerPlugin implements PluginInterface, EventSubscriberInterfa
         // GitLab
         $fs->removeDirectory('./.drainpipe/gitlab');
         if (isset($this->extra['drainpipe']['gitlab']) && is_array($this->extra['drainpipe']['gitlab'])) {
-            $fs->ensureDirectoryExists('./.drainpipe/gitlab');
-            $fs->copy("$scaffoldPath/gitlab/Common.gitlab-ci.yml", ".drainpipe/gitlab/Common.gitlab-ci.yml");
-            $this->io->write("🪠 [Drainpipe] .drainpipe/gitlab/Common.gitlab-ci.yml installed");
+            if (file_exists('./.ddev/config.yaml')) {
+                $fs->ensureDirectoryExists('.gitlab/drainpipe');
+                $fs->copy("$scaffoldPath/gitlab/DDEV.gitlab-ci.yml", ".gitlab/drainpipe/DDEV.gitlab-ci.yml");
+                $this->io->write("🪠 [Drainpipe] .gitlab/drainpipe/DDEV.gitlab-ci.yml installed");
+            }
+            else {
+                $fs->ensureDirectoryExists('./.drainpipe/gitlab');
+                $fs->copy("$scaffoldPath/gitlab/Common.gitlab-ci.yml", ".drainpipe/gitlab/Common.gitlab-ci.yml");
+                $this->io->write("🪠 [Drainpipe] .drainpipe/gitlab/Common.gitlab-ci.yml installed");
+            }
             foreach ($this->extra['drainpipe']['gitlab'] as $gitlab) {
                 $file = "gitlab/$gitlab.gitlab-ci.yml";
                 if (file_exists("$scaffoldPath/$file")) {
+                    $fs->ensureDirectoryExists('./.drainpipe/gitlab');
                     $fs->copy("$scaffoldPath/$file", ".drainpipe/$file");
                     $this->io->write("🪠 [Drainpipe] .drainpipe/$file installed");
                 }
