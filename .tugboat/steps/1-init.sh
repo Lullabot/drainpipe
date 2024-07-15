45cdc6cbaf01a965c6c1a625f3e6f69ecdde0398
David Burns <david@lullabot.com>
2024-07-15 09:09:52 -0400

    Issue #605: Pin Drupal 10.2.7

--- .tugboat/steps/1-init.sh
+++ .tugboat/steps/1-init.sh
@@ -74,7 +74,7 @@ nodejs -v | grep -q v$NODE_MAJOR
 shopt -s dotglob
 mkdir ../drainpipe-tmp
 mv * ../drainpipe-tmp/
-composer create-project drupal/recommended-project:~10.2 .
+composer create-project drupal/recommended-project:10.2.7 .
 mv ../drainpipe-tmp drainpipe
 composer config extra.drupal-scaffold.gitignore true
 composer config --json extra.drupal-scaffold.allowed-packages \[\"lullabot/drainpipe\"]
