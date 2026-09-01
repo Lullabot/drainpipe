fb97ed6950e6d52ed828e09452ac916be786a203
Zequi Vazquez <zequi@lullabot.com>
2026-09-01 21:01:06 +0200

    fix: convert package to ESM

--- metapackages/sass/gulp.js
+++ metapackages/sass/gulp.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 const gulp = require('gulp');
-const gulpfile = require('./gulpfile');
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
-if (argv.watch) {
-  gulp.task('sass:watch')();
-}
-else {
-  gulp.task('sass')();
-}
+
+// The gulpfile is ESM because some of its dependencies (e.g. cssnano) are
+// ESM-only, so it has to be loaded with a dynamic import.
+import('./gulpfile.mjs').then(() => {
+  if (argv.watch) {
+    gulp.task('sass:watch')();
+  }
+  else {
+    gulp.task('sass')();
+  }
+});
